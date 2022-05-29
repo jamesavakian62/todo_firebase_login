7e01d7c530bb4192835dce3c08385865cd194e98
James L Avakian <92414210+jamesavakian62@users.noreply.github.com>
2022-05-29 11:22:30 -0700

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+
+
+fuhnction Navbar(){
+    return (
+        <>
+     
+
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+
+<div className"w3-container">
+  <h2>Navigation BaNamers</h2>
+  <p>The <strong>w3-bar</strong> class is a container for displaying HTML elements horizontally.</p>
+  <p>The <strong>w3-bar-item</strong> class defines the container elements.</p>
+  <p>It it a perfect tool for creating navigation bars:</p>
+</div>
+
+<div className="w3-bar w3-black">
+  <a href="#" className="w3-bar-item w3-button">Home</a>
+  <a href="#" className="w3-bar-item w3-button">Link 1</a>
+  <a href="#" className="w3-bar-item w3-button">Link 2</a>
+  <a href="#" className="w3-bar-item w3-button">Link 3</a>
+</div>
+
+</>
+    )
+}
+
+export default Navbar;
